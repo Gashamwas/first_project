a6c7a66e8c2ab1c1a0b03fee34c99ad7c29048ff
Michael <wanjikumichaelgacha@gmail.com>
2017-10-14 23:16:04 +0300

    modifications

--- divide_two_numbers.py
+++ divide_two_numbers.py
@@ -1,12 +1,20 @@
 
+def cube(n):
+    return n**3
 
-def divide_two_numbers (x,y):
+def triangle(base, height)
+    return base*height/2
 
+import unittest
 
-    x = raw_input('Please enter a number')
-    y = raw_input('Please enter a number')
-   
-if x >=2
-    return x/y
-else:
-    print ('Please change the value of X: ')
+class myTest (unittest.TestCase);
+    def test_cube(self):
+        self-assertEqual (cube (5), 125)
+        self-assertEqual (cube (10), 100)
+        self-assertEqual (cube (0), 0)
+    def test-triangle(self)
+        self.testEqual (trangle(10,10), 50)
+        self.testEqual(triangle (5, 20), 50)
+        self-testEqual ( triangle (0, 50), 0)
+
+unittest.main()
